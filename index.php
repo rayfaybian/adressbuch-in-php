e786d3c71424a6cb1720362a3c1fefbc6bfb7fbb
sufa1987 <fabian.suppan@hotmail.com>
2021-05-02 10:16:05 +0200

    import anrede selection options from table "anrede"

--- index.php
+++ index.php
@@ -1,35 +1,48 @@
-<!--TEST COMMIT-->
+<?php include_once 'includes/dbhandler.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Adressbuch</title>
-  <link rel="stylesheet" href="styles/reset.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
-  <link rel="stylesheet" href="styles/style.css" />
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Adressbuch</title>
+    <link rel="stylesheet" href="styles/reset.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
+          integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
+          crossorigin="anonymous"/>
+    <link rel="stylesheet" href="styles/style.css"/>
 </head>
 
 <body>
-  <div class="background">
+<div class="background">
     <header>
-      <h1>Adressbuch</h1>
+        <h1>Adressbuch</h1>
     </header>
     <form class="input-area" action="includes/insert.php" method="POST">
-      <select name="anrede">
-        <option value="" selected disabled hidden>Anrede</option>
-        <option value="Herr">Herr</option>
-        <option value="Frau">Frau</option>
-      </select>
-      <input type="text" name="vorname" placeholder="Vorname" />
-      <input type="text" name="nachname" placeholder="Nachname" />
-      <input type="text" name="adresse" placeholder="Adresse" />
-      <input type="text" name="stadt" placeholder="Stadt" />
-      <input type="text" name="telefon" placeholder="Telefon" />
-      <button type="submit">Speichern</button>
+        <select name="anrede">
+            <option value="" selected disabled hidden>Anrede</option>
+
+            <?php
+
+            $sql = "SELECT * FROM anrede";
+            $result = mysqli_query($conn, $sql);
+            $resultCheck = mysqli_num_rows($result);
+            if ($resultCheck > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<option value=" . $row['anredeID'] . ">" . $row['anredeText'] . "</option>";
+                }
+            }
+            ?>
+
+        </select>
+        <input type="text" name="vorname" placeholder="Vorname"/>
+        <input type="text" name="nachname" placeholder="Nachname"/>
+        <input type="text" name="adresse" placeholder="Adresse"/>
+        <input type="text" name="stadt" placeholder="Stadt"/>
+        <input type="text" name="telefon" placeholder="Telefon"/>
+        <button type="submit">Speichern</button>
     </form>
 
     <?php
@@ -37,7 +50,7 @@
     (isset($_GET['order'])) ? $order = $_GET['order'] : $order = 'vorname';
     (isset($_GET['sort'])) ? $sort = $_GET['sort'] : $sort = 'ASC';
 
-    include_once 'includes/dbhandler.php';
+
     $sql = "SELECT * FROM adressbuch LEFT JOIN anrede ON adressbuch.anrede = anrede.anredeID ORDER BY $order $sort";
     $result = mysqli_query($conn, $sql);
     $resultCheck = mysqli_num_rows($result);
@@ -67,9 +80,9 @@
 
     /*Render Table Content if Database is not empty*/
     if ($resultCheck > 0) {
-      while ($row = mysqli_fetch_assoc($result)) {
-        echo
-        "<tr>
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo
+                "<tr>
         <td>" . $row['anredeText'] . "</td>
         <td>" . $row['vorname'] . "</td>
         <td>" . $row['nachname'] . "</td>
@@ -79,11 +92,11 @@
         <td><a href=includes/edit.php?id=" . $row['id'] . "><i class=\"fas fa-pen\"></a></i></td>
         <td><a href=includes/delete.php?id=" . $row['id'] . "><i class=\"fas fa-trash\"></a></i></td>
       </tr>";
-      }
+        }
     } ?>
     </table>
-  </div>
-  </div>
+</div>
+</div>
 
 </body>
 
