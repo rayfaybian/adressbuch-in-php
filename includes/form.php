203b6cb0c46e997c5919232d97b9950e923898b6
sufa1987 <fabian.suppan@hotmail.com>
2021-05-13 22:58:08 +0200

    Reformating code, casting "id" to integer

--- includes/form.php
+++ includes/form.php
@@ -6,13 +6,9 @@ include_once "dbhandler.php";
 $error_vorname = $error_nachname = $error_email = "";
 $error_class_vorname = $error_class_nachname = $error_class_email = "";
 
-/*GET ID FOR SQL QUERY*/
+/*SQL QUERY TO EDIT CONTACT*/
 if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-}
-
-/*FETCH DATA FOR EDITING*/
-if (isset($id)) {
+    $id = (int)$_GET['id'];
     $qry = mysqli_query($conn, "select * from adressbuch where id='$id'");
     $data = mysqli_fetch_array($qry);
 }
@@ -31,18 +27,8 @@ if (isset($_POST['update'])) {
         if (!mysqli_stmt_prepare($stmt, $edit)) {
             echo "SQL error";
         } else {
-            mysqli_stmt_bind_param(
-                $stmt,
-                "issssssi",
-                $newData['anrede'],
-                $newData['vorname'],
-                $newData['nachname'],
-                $newData['adresse'],
-                $newData['stadt'],
-                $newData['telefon'],
-                $newData['email'],
-                $id
-            );
+            mysqli_stmt_bind_param($stmt, "issssssi", $newData['anrede'], $newData['vorname'],
+                $newData['nachname'], $newData['adresse'], $newData['stadt'], $newData['telefon'], $newData['email'], $id);
             mysqli_stmt_execute($stmt);
         };
         /*RETURNING TO MAIN PAGE*/
@@ -65,23 +51,15 @@ if (isset($_POST['insert'])) {
         if (!mysqli_stmt_prepare($stmt, $sql)) {
             echo "SQL error";
         } else {
-            mysqli_stmt_bind_param(
-                $stmt,
-                "issssss",
-                $newData['anrede'],
-                $newData['vorname'],
-                $newData['nachname'],
-                $newData['adresse'],
-                $newData['stadt'],
-                $newData['telefon'],
-                $newData['email']
-            );
+            mysqli_stmt_bind_param($stmt, "issssss", $newData['anrede'], $newData['vorname'],
+                $newData['nachname'], $newData['adresse'], $newData['stadt'], $newData['telefon'], $newData['email']);
             mysqli_stmt_execute($stmt);
         }
 
         /*RETURNING TO MAIN PAGE*/
         header("Location: ../index.php");
     }
+
 }
 
 /*CREATING DATA ARRAY WITH NEW/UPDATED VALUES*/
@@ -95,15 +73,13 @@ function getNewData($conn)
     $telefon = mysqli_real_escape_string($conn, $_POST['telefon']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
 
-    return array(
-        "anrede" => $anrede,
+    return array("anrede" => $anrede,
         "vorname" => $vorname,
         "nachname" => $nachname,
         "adresse" => $adresse,
         "stadt" => $stadt,
         "telefon" => $telefon,
-        "email" => $email
-    );
+        "email" => $email);
 }
 
 /*CHECK IF REQUIRED FIELDS CONTAIN DATA*/
@@ -185,98 +161,98 @@ function checkUniqueMail($conn, $email)
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="stylesheet" href="../styles/reset.css">
-    <link rel="stylesheet" href="../styles/style.css" />
+    <link rel="stylesheet" href="../styles/style.css"/>
     <title>Kontakt Update</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
+          integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
+          crossorigin="anonymous"/>
 </head>
 
 <body>
-    <div class="form-background">
-        <header>
+<div class="form-background">
+    <header>
+        <?php
+        if (isset($data)) {
+            echo "<h1>Kontakt bearbeiten</h1>";
+        } else {
+            echo "<h1>Neuer Kontakt</h1>";
+        } ?>
+    </header>
+
+    <form class="input-area" method="POST">
+
+
+        <select name="anrede">
             <?php
-            if (isset($data)) {
-                echo "<h1>Kontakt bearbeiten</h1>";
-            } else {
-                echo "<h1>Neuer Kontakt</h1>";
-            } ?>
-        </header>
+            $sql = "SELECT * FROM anrede";
+            $result = mysqli_query($conn, $sql);
+            $resultCheck = mysqli_num_rows($result);
 
-        <form class="input-area" method="POST">
-
-
-            <select name="anrede">
-                <?php
-                $sql = "SELECT * FROM anrede";
-                $result = mysqli_query($conn, $sql);
-                $resultCheck = mysqli_num_rows($result);
-
-                if ($resultCheck > 0) {
-                    if (!isset($data)) {
-                        echo "<option value= '0' name='default' selected hidden>Anrede</option>";
-                    }
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        if ($row['anredeID'] == '0') {
-                            echo "<option value=" . $row['anredeID'] . " selected hidden>Anrede</option>";
-                        } else if ($data['anrede'] == $row['anredeID']) {
-                            echo "<option value=" . $row['anredeID'] . " selected >" . $row['anredeText'] . "</option>";
-                        } else {
-                            echo "<option value=" . $row['anredeID'] . ">" . $row['anredeText'] . "</option>";
-                        }
+            if ($resultCheck > 0)
+                while ($row = mysqli_fetch_assoc($result)) {
+                    if ($row['anredeID'] == '0') {
+                        echo "<option value=" . $row['anredeID'] . " selected hidden>Anrede</option>";
+                    } else if ($data['anrede'] == $row['anredeID']) {
+                        echo "<option value=" . $row['anredeID'] . " selected >" . $row['anredeText'] . "</option>";
+                    } else {
+                        echo "<option value=" . $row['anredeID'] . ">" . $row['anredeText'] . "</option>";
                     }
                 }
-                ?>
-            </select>
-
-            <input type="text" class="<?php echo $error_class_vorname ?>" name="vorname" value="<?php if (isset($data)) {
-                                                                                                    echo $data['vorname'];
-                                                                                                } else if (isset($newData)) {
-                                                                                                    echo $newData['vorname'];
-                                                                                                } ?>" placeholder="Vorname" />
-
-            <input type="text" name="nachname" class="<?php echo $error_class_nachname ?>" value="<?php if (isset($data)) {
-                                                                                                        echo $data['nachname'];
-                                                                                                    } else if (isset($newData)) {
-                                                                                                        echo $newData['nachname'];
-                                                                                                    } ?>" placeholder="Nachname" />
-            <input type="text" name="adresse" value="<?php if (isset($data)) {
-                                                            echo $data['adresse'];
-                                                        } else if (isset($newData)) {
-                                                            echo $newData['adresse'];
-                                                        } ?>" placeholder="Straße" />
-            <input type="text" name="stadt" value="<?php if (isset($data)) {
-                                                        echo $data['stadt'];
-                                                    } else if (isset($newData)) {
-                                                        echo $newData['stadt'];
-                                                    } ?>" placeholder="Stadt" />
-            <input type="text" name="telefon" value="<?php if (isset($data)) {
-                                                            echo $data['telefon'];
-                                                        } else if (isset($newData)) {
-                                                            echo $newData['telefon'];
-                                                        } ?>" placeholder="Telefon" />
-
-            <input type="text" name="email" class="<?php echo $error_class_email ?>" value="<?php if (isset($data)) {
-                                                                                                echo $data['email'];
-                                                                                            } else if (isset($newData)) {
-                                                                                                echo $newData['email'];
-                                                                                            } ?>" placeholder="E-Mail" />
-            <div class="error-label">
-                <label><?php echo $error_vorname ?></label>
-                <label><?php echo $error_nachname ?></label>
-                <label><?php echo $error_email ?></label>
-            </div>
-
-            <?php
-            if (isset($data)) {
-                echo "<button class=save-button type=submit name=update value=Update>Speichern</button>";
-            } else {
-                echo "<button class=save-button type=submit name=insert value=Insert>Speichern</button>";
-            }
             ?>
-        </form>
-        <button class="abort-button" onclick=location.href='../index.php'>Abbrechen</button>
-    </div>
+        </select>
+
+        <input type="text" class="<?php echo $error_class_vorname ?>" name="vorname"
+               value="<?php if (isset($data)) {
+                   echo $data['vorname'];
+               } else if (isset($newData)) {
+                   echo $newData['vorname'];
+               } ?>" placeholder="Vorname"/>
+
+        <input type="text" name="nachname"
+               class="<?php echo $error_class_nachname ?>" value="<?php if (isset($data)) {
+            echo $data['nachname'];
+        } else if (isset($newData)) {
+            echo $newData['nachname'];
+        } ?>" placeholder="Nachname"/>
+        <input type="text" name="adresse" value="<?php if (isset($data)) {
+            echo $data['adresse'];
+        } else if (isset($newData)) {
+            echo $newData['adresse'];
+        } ?>" placeholder="Straße"/>
+        <input type="text" name="stadt" value="<?php if (isset($data)) {
+            echo $data['stadt'];
+        } else if (isset($newData)) {
+            echo $newData['stadt'];
+        } ?>" placeholder="Stadt"/>
+        <input type="text" name="telefon" value="<?php if (isset($data)) {
+            echo $data['telefon'];
+        } else if (isset($newData)) {
+            echo $newData['telefon'];
+        } ?>" placeholder="Telefon"/>
+
+        <input type="text" name="email" class="<?php echo $error_class_email ?>" value="<?php if (isset($data)) {
+            echo $data['email'];
+        } else if (isset($newData)) {
+            echo $newData['email'];
+        } ?>" placeholder="E-Mail"/>
+        <div class="error-label">
+            <label><?php echo $error_vorname ?></label>
+            <label><?php echo $error_nachname ?></label>
+            <label><?php echo $error_email ?></label>
+        </div>
+
+        <?php
+        if (isset($data)) {
+            echo "<button class=save-button type=submit name=update value=Update>Speichern</button>";
+        } else {
+            echo "<button class=save-button type=submit name=insert value=Insert>Speichern</button>";
+        }
+        ?>
+    </form>
+    <button class="abort-button" onclick=location.href='../index.php'>Abbrechen</button>
+</div>
 </body>
