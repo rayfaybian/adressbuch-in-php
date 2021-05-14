6b5522d89564450cc65fb0695b279326eafadbb2
sufa1987 <fabian.suppan@hotmail.com>
2021-05-14 10:15:46 +0200

    - delete as function in index.php - removed unnecessary </ col> tags

--- index.php
+++ index.php
@@ -1,51 +1,49 @@
-<?php include_once 'includes/dbhandler.php'; ?>
+<?php include_once 'includes/dbhandler.php';
 
-<!DOCTYPE html>
-<html lang="en">
+if (isset($_GET['id'])) {
+    $id = (int)$_GET['id'];
+    deleteEntry($conn, $id);
+}
+?>
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Adressbuch</title>
-    <link rel="stylesheet" href="styles/reset.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
-          integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
-          crossorigin="anonymous"/>
-    <link rel="stylesheet" href="styles/style.css"/>
-</head>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Adressbuch</title>
+        <link rel="stylesheet" href="styles/reset.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
+              integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
+              crossorigin="anonymous"/>
+        <link rel="stylesheet" href="styles/style.css"/>
+    </head>
 
-<body>
-<div class="background">
-    <header>
-        <h1>Adressbuch</h1>
-    </header>
+    <body>
+    <div class="background">
+        <header>
+            <h1>Adressbuch</h1>
+        </header>
 
-    <button class="addButton" onclick=location.href='form.php'><i class="fas fa-plus fa-sm"></i> Neuer
-        Kontakt
-    </button>
+        <button class="addButton" onclick=location.href='form.php'><i class="fas fa-plus fa-sm"></i> Neuer
+            Kontakt
+        </button>
 
-    <?php
+        <?php
 
-    (isset($_GET['order'])) ? $order = $_GET['order'] : $order = 'vorname';
-    (isset($_GET['sort'])) ? $sort = $_GET['sort'] : $sort = 'ASC';
+        (isset($_GET['order'])) ? $order = $_GET['order'] : $order = 'vorname';
+        (isset($_GET['sort'])) ? $sort = $_GET['sort'] : $sort = 'ASC';
 
+        $sql = "SELECT * FROM adressbuch LEFT JOIN anrede ON adressbuch.anrede = anrede.anredeID ORDER BY $order $sort";
+        $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
 
-    $sql = "SELECT * FROM adressbuch LEFT JOIN anrede ON adressbuch.anrede = anrede.anredeID ORDER BY $order $sort";
-    $result = mysqli_query($conn, $sql);
-    $resultCheck = mysqli_num_rows($result);
+        $sort == 'DESC' ? $sort = 'ASC' : $sort = 'DESC';
 
-    $sort == 'DESC' ? $sort = 'ASC' : $sort = 'DESC';
-
-    /*Render Table Head*/
-    echo "<div class='table' id='table'>
+        /*Render Table Head*/
+        echo "<div class='table' id='table'>
     <table>
-      <col />
-      <col />
-      <col />
-      <col />
-      <col />
-      <colgroup></colgroup>
       <thead>
         <tr>
           <th><a href='?order=anredeText&&sort=$sort'>Anrede <span class='fas fa-sort'></span></a></th>
@@ -59,11 +57,11 @@
         </tr>
       </thead>";
 
-    /*Render Table Content if Database is not empty*/
-    if ($resultCheck > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo
-                "<tr>
+        /*Render Table Content if Database is not empty*/
+        if ($resultCheck > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo
+                    "<tr>
         <td>" . $row['anredeText'] . "</td>
         <td>" . $row['vorname'] . "</td>
         <td>" . $row['nachname'] . "</td>
@@ -71,15 +69,21 @@
         <td>" . $row['stadt'] . "</td>
         <td>" . $row['telefon'] . "</td>
         <td>" . $row['email'] . "</td>
-        <td><a href=form.php?id=" . $row['id'] . "><i class=\"fas fa-pen\"></a></i></td>
-        <td><a href=includes/delete.php?id=" . $row['id'] . "><i class=\"fas fa-trash\"></a></i></td>
+        <td><a href=form.php?id=" . $row['id'] . "><i class=\"fas fa-pen\"></a></i></td>      
+        <td><a href=index.php?id=" . $row['id'] . "><i class=\"fas fa-trash\"></a></i></td>
       </tr>";
+            }
         }
-    } ?>
-    </table>
-</div>
-</div>
+        ?>
+        </table>
+    </div>
+    </div>
 
-</body>
+    </body>
 
-</html>
+    </html>
+
+<?php function deleteEntry($conn, $id)
+{
+    mysqli_query($conn, "delete from adressbuch where id = '$id'");
+} ?>
